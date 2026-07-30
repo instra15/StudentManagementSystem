28c2194b4f4b1d00d480d19c296144aa9647589b
instra15 <a3026164589@outlook.com>
2026-07-30 20:05:57 +0800

    Initial commit

---
+++ src/main/java/com/example/studentmanagementsystem/DAO/Student.java
@@ -0,0 +1,15 @@
+package com.example.studentmanagementsystem.DAO;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "student")
+@Data
+public class Student {
+
+
+
+
+}
