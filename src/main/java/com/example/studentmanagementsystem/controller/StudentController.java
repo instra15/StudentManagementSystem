da70c1fc4f858b16de1290532a4f247284c397d0
instra15 <a3026164589@outlook.com>
2026-07-30 21:26:24 +0800

    1

--- src/main/java/com/example/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/example/studentmanagementsystem/controller/StudentController.java
@@ -1,7 +1,35 @@
 package com.example.studentmanagementsystem.controller;
 
+import com.example.studentmanagementsystem.DAO.Student;
+import com.example.studentmanagementsystem.Response;
+import com.example.studentmanagementsystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/student/find/{id}")
+    public Response<Student> SearchStudentById(@PathVariable long id)
+    {
+        return studentService.getStudentById(id);
+    }
+
+    @GetMapping("/student/find/{name}")
+    public Response<Student> SearchStudentByName(@PathVariable String name)
+    {
+        return studentService.getStudentByName(name);
+    }
+
+    @GetMapping("/student/find/{no}")
+    public Response<Student> SearchStudentByNo(@PathVariable String no)
+    {
+        return studentService.getStudentByStudentNo(no);
+    }
+
 }
--- src/main/java/com/example/studentmanagementsystem/service/StudentServiceImpl.java
+++ src/main/java/com/example/studentmanagementsystem/service/StudentServiceImpl.java
@@ -6,7 +6,9 @@ import com.example.studentmanagementsystem.Response;
 import com.example.studentmanagementsystem.exception.StudentAlreadyExist;
 import com.example.studentmanagementsystem.exception.StudentNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class StudentServiceImpl implements StudentService{
 
     @Autowired
